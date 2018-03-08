b244986ea3e2ece3b2eb55b3bde128fe7bf015f2
James Powers <powersajames@gmail.com>
2018-03-08 11:35:57 -0800

    fully functional proxy server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+
+app.use('/listings/:id', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
